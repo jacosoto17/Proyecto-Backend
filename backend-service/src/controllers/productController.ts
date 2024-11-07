0b331f83c5e27340ff65821ce103e58e402adde2
jacosoto17 <jacosoto17@gmail.com>
2024-11-07 09:08:20 -0500

    Proyecto Backend

---
+++ backend-service/src/controllers/productController.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Product } from "../entities/Ropa";
+
+const productRepository = AppDataSource.getRepository(Product);
+
+// GET - Obtener Todos los Productos
+export const getAllProducts = async(red: Request, res: Response) => {
+  try {
+    const products = await productRepository.find();
+    res.json(products);
+  } catch(error) {
+    res.status(500).json({ message: "Error al obtener productos." });
+  }
+};
+
+// GET by ID - Obetener Producto por ID
+export const getProductById = async(req: Request, res: Response) => {
+  try {
+    const product = await productRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if(product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Producto no encontrado" });
+    }
+  } catch(error) {
+    res.status(500).json({ message: "Error al obtener el producto." });
+  }
+};
+
+// POST - Crear un nuevo Producto
+export const createProduct = async(req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body;
+    const product = new Product();
+    product.name = name;
+    product.description = description;
+    product.price = price;
+
+    await productRepository.save(product);
+    res.status(201).json(product);
+  } catch(error) {
+    res.status(500).json({ message: "Error al crear el producto." });
+  }
+};
+
+// PUT - Actualizar un Producto existente
+export const updateProduct = async(req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body;
+    const product = await productRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if(product) {
+      product.name = name ?? product.name;
+      product.description = description ?? product.description;
+      product.price = price ?? product.price;
+
+      await productRepository.save(product);
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Producto no encontrado" });
+    }
+  } catch(error) {
+    res.status(500).json({ message: "Error al actualizar el producto." });
+  }
+};
+
+// DELETE - Borrar un Producto
+export const deleteProduct = async(req: Request, res: Response) => {
+  try {
+    const product = await productRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if (product) {
+      await productRepository.remove(product);
+      res.json({ message: "Producto eliminado." });
+    } else {
+      res.status(404).json({ message: "Producto no encontrado." });
+    }
+  } catch(error) {
+    res.status(500).json({ message: "Error al eliminar el producto." });
+  }
+};
